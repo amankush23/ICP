90664d3d532d5b14df61923a5f71ecb5227ad228
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-09-30 22:51:01 +0530

    Create 3_lengthOfLongestSubstring.java

---
+++ HOME_ASSIGNMENT_04/3_lengthOfLongestSubstring.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return LLS(s);
+    }
+    public static int LLS(String s) {
+        int n = s.length();
+        HashSet<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
