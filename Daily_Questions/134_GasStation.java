1b7389925d520fd6be1b3715fd03cc7ded51d164
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-02 23:35:26 +0530

    Implement gas station circuit solution

---
+++ Daily_Questions/134_GasStation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        return canComplete(gas, cost);
+    }
+    public static int canComplete(int[] gas, int[] cost){
+        int total = 0;
+        for(int i =0; i < cost.length ; i++){
+            total += gas[i] - cost[i];
+        }
+        if(total < 0){
+            return -1; 
+        }
+        int idx =0;
+        int curr =0;
+        for(int i = 0; i < cost.length; i++){
+            curr += gas[i] - cost[i];
+            if(curr < 0){
+                curr = 0; 
+                idx = i+1;
+            }
+    }return idx;
+    
+            
+    }
+}
