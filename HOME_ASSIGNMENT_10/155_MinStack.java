5ab43f022b5098493c3feaa3390663f7742689ad
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-10-21 21:57:56 +0530

    Implement MinStack with push, pop, top, and getMin

---
+++ HOME_ASSIGNMENT_10/155_MinStack.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class MinStack {
+    private List<Integer> ll;  // make it a class variable
+    private int size;
+
+    public MinStack() {
+        ll = new ArrayList<>();
+        size = 0;
+    }
+    
+    public void push(int val) {
+        ll.add(val);
+        size++;
+    }
+    
+    public void pop() {
+        if (!ll.isEmpty()) {
+            ll.remove(ll.size() - 1);
+            size--;
+        }
+    }
+    
+    public int top() {
+        if (!ll.isEmpty()) {
+            return ll.get(ll.size() - 1);
+        }
+        throw new EmptyStackException();
+    }
+    
+    public int getMin() {
+        int min = Integer.MAX_VALUE;
+        for (int val : ll) {
+            min = Math.min(min, val);
+        }
+        return min;
+    }
+}
